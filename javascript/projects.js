c03409858af4e5bc1201795fa7dd45cafde297d0
ariefyud <ariefyud12@gmail.com>
2021-04-01 06:23:26 +0700

    update projects display

--- javascript/projects.js
+++ javascript/projects.js
@@ -11,58 +11,77 @@ img3.onmouseover = img3.onmouseout = changeImage3;
 img4.onmouseover = img4.onmouseout = changeImage4;
 img5.onmouseover = img5.onmouseout = changeImage5;
 img6.onmouseover = img6.onmouseout = changeImage6;
-
+img7.onmouseover = img7.onmouseout = changeImage7;
+// img8.onmouseover = img8.onmouseout = changeImage8;
 
 function changeImage1(e) {
   if (event.type == 'mouseover') {
-    document.getElementById('img1').src ='img/color-1.jpg'
+    document.getElementById('img1').src = './img/color-1.jpg';
   }
   if (event.type == 'mouseout') {
-    document.getElementById('img1').src ='img/black-1.jpg'
+    document.getElementById('img1').src = './img/black-1.jpg';
   }
 }
 
 function changeImage2(e) {
   if (event.type == 'mouseover') {
-    document.getElementById('img2').src ='img/color-2.jpg'
+    document.getElementById('img2').src = './img/color-2.jpg';
   }
   if (event.type == 'mouseout') {
-    document.getElementById('img2').src ='img/black-2.jpg'
+    document.getElementById('img2').src = './img/black-2.jpg';
   }
 }
 
 function changeImage3(e) {
   if (event.type == 'mouseover') {
-    document.getElementById('img3').src ='img/color-3.jpg'
+    document.getElementById('img3').src = './img/color-3.jpg';
   }
   if (event.type == 'mouseout') {
-    document.getElementById('img3').src ='img/black-3.jpg'
+    document.getElementById('img3').src = './img/black-3.jpg';
   }
 }
 
 function changeImage4(e) {
   if (event.type == 'mouseover') {
-    document.getElementById('img4').src ='img/color-4.jpg'
+    document.getElementById('img4').src = './img/color-4.jpg';
   }
   if (event.type == 'mouseout') {
-    document.getElementById('img4').src ='img/black-4.jpg'
+    document.getElementById('img4').src = './img/black-4.jpg';
   }
 }
 
 function changeImage5(e) {
   if (event.type == 'mouseover') {
-    document.getElementById('img5').src ='img/color-5.jpg'
+    document.getElementById('img5').src = './img/color-5.jpg';
   }
   if (event.type == 'mouseout') {
-    document.getElementById('img5').src ='img/black-5.jpg'
+    document.getElementById('img5').src = './img/black-5.jpg';
   }
 }
 
 function changeImage6(e) {
   if (event.type == 'mouseover') {
-    document.getElementById('img6').src ='img/color-6.jpg'
+    document.getElementById('img6').src = './img/color-6.jpg';
   }
   if (event.type == 'mouseout') {
-    document.getElementById('img6').src ='img/black-6.jpg'
+    document.getElementById('img6').src = './img/black-6.jpg';
   }
 }
+
+function changeImage7(e) {
+  if (event.type == 'mouseover') {
+    document.getElementById('img7').src = './img/color-7.jpg';
+  }
+  if (event.type == 'mouseout') {
+    document.getElementById('img7').src = './img/black-7.jpg';
+  }
+}
+
+// function changeImage8(e) {
+//   if (event.type == 'mouseover') {
+//     document.getElementById('img8').src = 'img/color-8.jpg';
+//   }
+//   if (event.type == 'mouseout') {
+//     document.getElementById('img8').src = 'img/black-8.jpg';
+//   }
+// }
--- javascript/toggle.js
+++ javascript/toggle.js
@@ -8,49 +8,67 @@ function visible1() {
 }
 
 function visible2() {
-  let text1 = document.getElementById('text-2');
-  if (text1.style.top === '-50%') {
-    text1.style.top = '25%';
+  let text2 = document.getElementById('text-2');
+  if (text2.style.top === '-100%') {
+    text2.style.top = '25%';
   } else {
-    text1.style.top = '-50%';
+    text2.style.top = '-100%';
   }
 }
 
 function visible3() {
-  let text1 = document.getElementById('text-3');
-  if (text1.style.top === '-50%') {
-    text1.style.top = '25%';
+  let text3 = document.getElementById('text-3');
+  if (text3.style.top === '-50%') {
+    text3.style.top = '25%';
   } else {
-    text1.style.top = '-50%';
+    text3.style.top = '-50%';
   }
 }
 
 function visible4() {
-  let text1 = document.getElementById('text-4');
-  if (text1.style.top === '-50%') {
-    text1.style.top = '25%';
+  let text4 = document.getElementById('text-4');
+  if (text4.style.top === '-50%') {
+    text4.style.top = '25%';
   } else {
-    text1.style.top = '-50%';
+    text4.style.top = '-50%';
   }
 }
 
 function visible5() {
-  let text1 = document.getElementById('text-5');
-  if (text1.style.top === '-50%') {
-    text1.style.top = '25%';
+  let text5 = document.getElementById('text-5');
+  if (text5.style.top === '-50%') {
+    text5.style.top = '25%';
   } else {
-    text1.style.top = '-50%';
+    text5.style.top = '-50%';
   }
 }
 
 function visible6() {
-  let text1 = document.getElementById('text-6');
-  if (text1.style.top === '-50%') {
-    text1.style.top = '25%';
+  let text6 = document.getElementById('text-6');
+  if (text6.style.top === '-50%') {
+    text6.style.top = '25%';
   } else {
-    text1.style.top = '-50%';
+    text6.style.top = '-50%';
   }
 }
+
+function visible7() {
+  let text7 = document.getElementById('text-7');
+  if (text7.style.top === '-50%') {
+    text7.style.top = '25%';
+  } else {
+    text7.style.top = '-50%';
+  }
+}
+
+// function visible8() {
+//   let text8 = document.getElementById('text-8');
+//   if (text8.style.top === '-50%') {
+//     text8.style.top = '25%';
+//   } else {
+//     text8.style.top = '-50%';
+//   }
+// }
 
 // function blur() {
 //   let img1 = document.querySelector('#img1');
